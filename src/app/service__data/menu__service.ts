4c3829b4114a8ff39d32de0154c1d851b2a6ff24
LuongMinh13 <l.minh1995@yahoo.fr>
2021-09-19 21:32:45 +0200

    change url image

--- src/app/service__data/menu__service.ts
+++ src/app/service__data/menu__service.ts
@@ -9,17 +9,17 @@ export class MenuServices {
     getMenu(): IMenu[] {
         return [
             {
-                "ImageMenu": "../../assets/img/menu/PhoBo.png",
+                "ImageMenu": "././assets/img/menu/PhoBo.png",
                 "MenuTitle": "Pho Chez Ha My",
                 "MenuSubtitle": "Soupe composée d'un bouillon clair à base d'os à moelle et de viande de boeuf, des lamelles de boeuf, d'oignon, des épices, du nuoc mam et de pâtes plates de riz accompagnée d'herbes aromatiques."
             },
             {
-                "ImageMenu": "../../assets/img/menu/BoBunTom.png",
+                "ImageMenu": "././assets/img/menu/BoBunTom.png",
                 "MenuTitle": "Bo Bun Crevette",
                 "MenuSubtitle": "Composé de vermicelles de riz accompagnés de germes de soja, de crevette sauté aux oignons, de nems, de concombre en julienne, cacahuète. Accompagnée le menthe et coriandre"
             },
             {
-                "ImageMenu": "../../assets/img/menu/BoLaLot.png",
+                "ImageMenu": "././assets/img/menu/BoLaLot.png",
                 "MenuTitle": "Boeuf La Lot",
                 "MenuSubtitle": "Les Bo la lot sont des rouleaux vietnamiens de bœuf à la citronnelle aux vermicelles de riz. La viande de bœuf est marinée dans du jus d’orange et roulé dans des feuilles de bétel et accompagnée d'herbes aromatiques."
             }
--- src/app/service__data/service__service.ts
+++ src/app/service__data/service__service.ts
@@ -9,17 +9,17 @@ export class ServicesService{
     getServices(): IService[]{
         return[
             {
-                "ServicesIcon" : "../../assets/icon/cooking (1).png",
+                "ServicesIcon" : "././assets/icon/cooking (1).png",
                 "ServicesTitle": "Excellent cuisine",
                 "ServicesSubtitle" : "Pour votre bonheur nous avons sélectionné des meilleurs plats, cuisiner par le meilleur soin de notre chef."
             },
             {
-                "ServicesIcon" : "../../assets/icon/meat (1).png",
+                "ServicesIcon" : "././assets/icon/meat (1).png",
                 "ServicesTitle" :"Meilleur ingrédient",
                 "ServicesSubtitle" : "Les plats cuisinés entièrement sur place à partir de produits bruts. Les produits ont sélectionné par nos soins."
             },
             {
-                "ServicesIcon" : "../../assets/icon/package.png",
+                "ServicesIcon" : "././assets/icon/package.png",
                 "ServicesTitle" : "Livraison à votre domicile",
                 "ServicesSubtitle" : "Retrouvez-vous sur l'application Deliveroo, ou simplement de nous appeler pour faites livrer votre plats à votre domicile",
             }
